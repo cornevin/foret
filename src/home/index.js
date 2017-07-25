066bea43f5d09d833bc5def3f9946cd4542255a5
cornevin <quentin.cornevin@accenture.com>
2017-07-25 21:40:27 +0200

    add login with username

--- src/home/index.js
+++ src/home/index.js
@@ -4,30 +4,52 @@ import './style.css'
 import { withRouter } from 'react-router-dom'
 import RaisedButton from 'material-ui/RaisedButton'
 import * as firebase from '../core/firebase'
+import TextField from 'material-ui/TextField'
 
 class Home extends Component {
 
   constructor(props) {
     super(props)
     this.joinRoom = this.joinRoom.bind(this)
-    const rootRef = firebase.database.ref().child('foret')
+    this.state = {
+      userName: '',
+    }
   }
 
   joinRoom() {
-    this.props.history.push("/room");
+    const rootRef = firebase.database.ref().child('foret')
+    rootRef.child('users').push(this.state.userName)
+    if (this.state.userName !== '') {
+      this.setState({
+        userName: ''
+      })
+      this.props.history.push("/room");
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      userName: event.target.value
+    })
   }
 
   render() {
     return (
-    <div className="home">
-      <img src={tree} alt="app logo" style={{ height: 200 }} />
-      <div>
-        <h1> Welcome to foret </h1>
+      <div className="home">
+        <img src={tree} alt="app logo" style={{ height: 200 }} />
+        <div>
+          <h1> Welcome to foret </h1>
+        </div>
+        <TextField
+          value={this.state.userName}
+          onChange={this.handleChange}
+          hintText="Enter your username ..."
+          floatingLabelStyle={{ color: "#FFFFFF" }}
+        />
+        <div onClick={this.joinRoom}>
+          <RaisedButton label="Join the Room" labelColor="#FFFFFF" backgroundColor="#20855D" style={{ margin: 12 }} />
+        </div>
       </div>
-      <div onClick={this.joinRoom}>
-        <RaisedButton label="Join the Room" labelColor="#FFFFFF" backgroundColor="#20855D" style={{margin: 12}} />
-      </div>
-    </div>
     )
   }
 }
